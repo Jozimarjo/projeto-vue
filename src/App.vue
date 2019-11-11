0b038231ce6cfd1a6b37e4fb5bac19ecc8f7c37c
jozimar <jozimarjo@gmail.com>
2019-11-11 02:24:58 -0200

    pwa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,231 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app id="inspire">
+
+        <v-content>
+            <v-container>
+                <v-btn color="success" @click="btn = !btn">teste
+                    <v-icon>mdi-plus</v-icon>
+                </v-btn>
+            </v-container>
+        </v-content>
+        <v-content>
+            <v-container>
+                <h1>Motorola</h1>
+                <v-card>
+                    <v-img class="white--text align-end" height="375px"
+                           src="https://images-na.ssl-images-amazon.com/images/G/32/br-marketing-coop/Setembro/Motorola/Banner_Amazon_1500x375_BrandPage_Desk._CB436436140_.png">
+                        <v-card-title>Top 10 Celulares motorolas</v-card-title>
+                    </v-img>
+
+                </v-card>
+            </v-container>
+            <HelloWorld v-if="btn"/>
+            <CardHorizontal v-if="!btn"/>
+        </v-content>
+        <!--      // cortana ficar por baixo-->
+        <v-navigation-drawer disable-resize-watcher="true" v-model="drawer" clipped="true" app>
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-row v-if="item.heading" :key="item.heading" align="center">
+                        <v-col cols="6">
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-col>
+                        <v-col cols="6" class="text-center">
+                            <a href="#!" class="body-2 black--text">EDIT</a>
+                        </v-col>
+                    </v-row>
+                    <v-list-group v-else-if="item.children" :key="item.text" v-model="item.model"
+                                  :prepend-icon="item.model ? item.icon : item['icon-alt']" append-icon="">
+                        <template v-slot:activator>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ item.text }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
+
+                        <v-list-item v-for="(child, i) in item.children" :key="i" link>
+                            <v-list-item-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ child.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                    <v-list-item v-else :key="item.text" link>
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{ item.text }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
+        <!--      // toolbar em toda tela-->
+        <v-app-bar clipped-left="true" app color="blue darken-3" dark>
+            <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+                <span >CollorMaq Etiquetas</span>
+            </v-toolbar-title>
+            <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search"
+                          class="hidden-sm-and-down"/>
+            <v-spacer/>
+            <v-btn icon>
+                <v-icon>mdi-apps</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-bell</v-icon>
+            </v-btn>
+            <v-btn icon large>
+                <v-avatar size="32px" item>
+                    <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"/>
+                </v-avatar>
+            </v-btn>
+        </v-app-bar>
+        <v-content>
+            <v-container class="fill-height" fluid>
+
+            </v-container>
+        </v-content>
+        <v-btn bottom color="pink" dar fab fixed right @click="dialog = !dialog">
+            <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <v-dialog v-model="dialog" width="800px">
+            <v-card>
+                <v-card-title class="grey darken-2">
+                    Create contact
+                </v-card-title>
+                <v-container>
+                    <v-row>
+                        <v-col class="align-center justify-space-between" cols="12"
+                        >
+                            <v-row align="center">
+                                <v-avatar size="40px" class="mr-4">
+                                    <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt="">
+                                </v-avatar>
+                                <v-text-field placeholder="Name"/>
+                            </v-row>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field prepend-icon="business" placeholder="Company"/>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field placeholder="Job title"/>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field prepend-icon="mail" placeholder="Email"/>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field type="tel" prepend-icon="phone" placeholder="(000) 000 - 000"
+                            />
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field prepend-icon="notes" placeholder="Notes"/>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <v-card-actions>
+                    <v-btn text color="primary">More</v-btn>
+                    <v-spacer/>
+                    <v-btn text color="primary" @click="dialog = false">Cancel</v-btn>
+                    <v-btn text @click="dialog = false">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    /* eslint-disable */
+    import HelloWorld from './components/HelloWorld';
+    import CardHorizontal from './components/card-horizontal'
+    //
+    export default {
+        name: 'App',
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+        components: {
+            HelloWorld,
+            CardHorizontal
+        },
+
+        data: () => ({
+            dialog: false,
+            drawer: null,
+            btn: false,
+            pessoa: [
+                {nome: 'Jozimar', idade: 23, cpf: '123456789'},
+                {nome: 'kevin', idade: 23, cpf: '123456789'},
+                {nome: 'deyveson', idade: 23, cpf: '123456789'},
+                {nome: 'lulu', idade: 23, cpf: '123456789'}],
+            teste: {},
+            pos: '',
+            nome: '',
+            items: [
+                {icon: 'mdi-contacts', text: 'Contacts'},
+                {icon: 'mdi-history', text: 'Frequently contacted'},
+                {icon: 'mdi-content-copy', text: 'Duplicates'},
+                {
+                    icon: 'mdi-chevron-up',
+                    'icon-alt': 'mdi-chevron-down',
+                    text: 'Labels',
+                    model: true,
+                    children: [
+                        {icon: 'mdi-plus', text: 'Create label'},
+                    ],
+                },
+                {
+                    icon: 'mdi-chevron-up',
+                    'icon-alt': 'mdi-chevron-down',
+                    text: 'More',
+                    model: false,
+                    children: [
+                        {text: 'Import'},
+                        {text: 'Export'},
+                        {text: 'Print'},
+                        {text: 'Undo changes'},
+                        {text: 'Other contacts'},
+                    ],
+                },
+                {icon: 'mdi-settings', text: 'Settings'},
+                {icon: 'mdi-message', text: 'Send feedback'},
+                {icon: 'mdi-help-circle', text: 'Help'},
+                {icon: 'mdi-cellphone-link', text: 'App downloads'},
+                {icon: 'mdi-keyboard', text: 'Go to the old version'},
+            ],
+            //
+        }),
+        methods: {
+            edit(item) {
+
+                console.log(item)
+                const index = this.pessoa.findIndex(value => value.nome === item.nome)
+                console.log('A posicao e ', this.pessoa[index].nome)
+                this.teste = this.pessoa[index];
+
+                // this.teste = this.pessoa[0];
+
+
+            },
+            teste1() {
+                console.log(this.nome)
+
+                let x = {nome: '', idade: '21', cpf: '12212121'}
+                x.nome = this.nome;
+                this.pessoa.push(x);
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/card-horizontal.vue
+++ src/components/card-horizontal.vue
@@ -1,13 +1,137 @@
 <template>
-    
-</template>
 
+    <v-container>
+
+        <v-card class="mx-auto teste" v-for="item in pessoa" :key="item.nome" @click="clicks()">
+            <!--            <v-container>-->
+            <v-row justify="space-between">
+                <v-col cols="auto">
+                    <v-img height="auto" width="auto" :src="item.img"></v-img>
+                </v-col>
+                <v-col class="text-center pl-0">
+                    <v-row class="flex-column ma-0 fill-height" justify="center">
+                        <!--                        <v-list-item-title class="headline"> Our Changing Planet</v-list-item-title>-->
+                        <!--                        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>-->
+                        <v-col class="px-0">
+                            <v-card-title>{{item.nome}}--SMEU PRIMEIRO TESTE</v-card-title>
+                            <v-card-subtitle>por Motorola</v-card-subtitle>
+                        </v-col>
+
+                        <!--                        <v-col class="px-0">-->
+                        <!--                            <v-list-item-title class="headline"> 2Our Changing Planet</v-list-item-title>-->
+                        <!--                            <v-list-item-subtitle>2by Kurt Wagner</v-list-item-subtitle>-->
+                        <!--                        </v-col>-->
+
+                        <!--                        <v-col class="px-0">-->
+                        <!--                            <v-btn icon>-->
+                        <!--                                <v-icon>mdi-share-variant</v-icon>-->
+                        <!--                            </v-btn>-->
+                        <!--                        </v-col>-->
+                    </v-row>
+                </v-col>
+            </v-row>
+            <!--            </v-container>-->
+        </v-card>
+    </v-container>
+</template>
 <script>
     export default {
-        name: "card-horizontal"
+        name: 'CardoHorizontal',
+        props: {},
+        watch: {},
+        data: () => ({
+            pessoa: [
+                {
+                    nome: 'Smartphone, Motorola, Motorola One, XT1941-3, 64 GB, 5.9", Branco\n',
+                    idade: 23,
+                    cpf: '123456789',
+                    img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+                },
+                {
+                    nome: 'Smartphone Motorola One Action XT2013-1, 128 GB, 6.34\'\', Branco Polar',
+                    idade: 23,
+                    cpf: '123456789',
+                    img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                },
+                {
+                    nome: 'Fone de Ouvido Pulse Max com Microfone, Motorola, SH004, Preto',
+                    idade: 23,
+                    cpf: '123456789',
+                    img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+                },
+                {
+                    nome: 'Smartphone, Motorola, Moto G6, XT1925, 64 GB, 5.7", Preto',
+                    idade: 23,
+                    cpf: '123456789',
+                    img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+                },
+                {
+                    nome: 'Smartphone, Motorola, G7 Power XT1955-1, 64GB, 6.2\'\', Lilás',
+                    idade: 23,
+                    cpf: '123456789',
+                    img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+                },
+                {
+                    nome: 'iphone X Apple Cinza Espacial, Tela de 5,8", 4G, 64 GB, Câmera de 12 MP',
+                    idade: 23,
+                    cpf: '123456789',
+                    img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+                },
+                {
+                    nome: 'iphone X Apple Cinza Espacial, Tela de 5,8", 4G, 256 GG, Câmera de 12 MP',
+                    idade: 23,
+                    cpf: '123456789', img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                },
+                {
+                    nome: 'Apple Iphone X Prateado 64Gb Com 5,8, Ios 11, Processador A11 64 Bits, Cã¢Mera Dupla De 12 Mp Desbloqueado',
+                    idade: 23,
+                    cpf: '123456789', img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                },
+                {
+                    nome: 'Iphone X 64gb Cinza Espacial Space Gray Tela 5.8  IOS 11 4G 12MP Apple',
+                    idade: 23,
+                    cpf: '123456789', img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                },
+                {
+                    nome: 'Smartphone Samsung Galaxy A10, 32Gb, Tela 6.2\'\', Azul, Sm-A105Mzbkzto',
+                    idade: 23,
+                    cpf: '123456789', img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                },
+                {
+                    nome: 'Smartphone, Samsung, Galaxy Note 9 SM-N9600ZKJZTO, 128 GB, 6.4\'\', Preto',
+                    idade: 23,
+                    cpf: '123456789', img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                },
+                {
+                    nome: 'Smartphone Samsung Galaxy A20, 32Gb, Tela 6.4\'\', Azul, Sm-A205Gzbjzto',
+                    idade: 23,
+                    cpf: '123456789', img: 'https://images-na.ssl-images-amazon.com/images/I/61qdEFm7fGL._US133_.jpg'
+
+                }
+            ],
+            teste: {},
+            pos: '',
+            nome: '',
+            cards: [
+                {title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12},
+                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6},
+                {title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6},
+            ],
+        }),
+        methods: {
+            clicks() {
+            }
+        }
     }
 </script>
-
-<style scoped>
-
+<style>
+    .teste {
+        margin-bottom: 1%;
+    }
 </style>
